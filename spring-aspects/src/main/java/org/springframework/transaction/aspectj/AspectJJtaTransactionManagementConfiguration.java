f6ff7da3069830c85d5a243ae271ba609fa0fd57
yoiekjcu <YoiEkjcU@outlook.com>
2021-01-23 17:09:32 +0800

    spring-aspects

--- spring-aspects/src/main/java/org/springframework/transaction/aspectj/AspectJJtaTransactionManagementConfiguration.java
+++ spring-aspects/src/main/java/org/springframework/transaction/aspectj/AspectJJtaTransactionManagementConfiguration.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.transaction.aspectj;
 
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -31,9 +15,9 @@ import org.springframework.transaction.config.TransactionManagementConfigUtils;
  * {@link org.springframework.transaction.annotation.Transactional} annotation.
  *
  * @author Juergen Hoeller
- * @since 5.1
  * @see EnableTransactionManagement
  * @see TransactionManagementConfigurationSelector
+ * @since 5.1
  */
 @Configuration
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
@@ -48,5 +32,4 @@ public class AspectJJtaTransactionManagementConfiguration extends AspectJTransac
 		}
 		return txAspect;
 	}
-
 }
