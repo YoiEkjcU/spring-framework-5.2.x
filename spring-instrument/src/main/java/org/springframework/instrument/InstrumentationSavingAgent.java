65defe1412cb748b77f63ec8b8b56c11d1201bbc
yoiekjcu <YoiEkjcU@outlook.com>
2021-01-23 17:49:18 +0800

    spring-instrument

--- spring-instrument/src/main/java/org/springframework/instrument/InstrumentationSavingAgent.java
+++ spring-instrument/src/main/java/org/springframework/instrument/InstrumentationSavingAgent.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.instrument;
 
 import java.lang.instrument.Instrumentation;
@@ -24,8 +8,8 @@ import java.lang.instrument.Instrumentation;
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
- * @since 2.0
  * @see org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver
+ * @since 2.0
  */
 public final class InstrumentationSavingAgent {
 
@@ -58,6 +42,7 @@ public final class InstrumentationSavingAgent {
 	 * conditional checking with respect to agent availability, consider using
 	 * {@link org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver#getInstrumentation()}
 	 * instead - which will work without the agent class in the classpath as well.
+	 *
 	 * @return the {@code Instrumentation} instance previously saved when
 	 * the {@link #premain} or {@link #agentmain} methods was called by the JVM;
 	 * will be {@code null} if this class was not used as Java agent when this
