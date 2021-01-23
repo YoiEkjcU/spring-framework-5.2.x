380a67b406a7efc40bc6565794879ab36c600fe5
yoiekjcu <YoiEkjcU@outlook.com>
2021-01-23 17:32:10 +0800

    spring-context-indexer

--- spring-context-indexer/src/main/java/org/springframework/context/index/processor/MetadataStore.java
+++ spring-context-indexer/src/main/java/org/springframework/context/index/processor/MetadataStore.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2018 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.context.index.processor;
 
 import java.io.IOException;
@@ -45,8 +29,7 @@ class MetadataStore {
 	public CandidateComponentsMetadata readMetadata() {
 		try {
 			return readMetadata(getMetadataResource().openInputStream());
-		}
-		catch (IOException ex) {
+		} catch (IOException ex) {
 			// Failed to read metadata -> ignore.
 			return null;
 		}
@@ -64,8 +47,7 @@ class MetadataStore {
 	private CandidateComponentsMetadata readMetadata(InputStream in) throws IOException {
 		try {
 			return PropertiesMarshaller.read(in);
-		}
-		finally {
+		} finally {
 			in.close();
 		}
 	}
